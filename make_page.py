4c40e8c34a52d05a6dfc55f61dcf5c7ffe8bf8a5
Igor Babuschkin <igor@babuschk.in>
2015-11-08 13:53:09 +0000

    Add bootstrap

--- make_page.py
+++ make_page.py
@@ -6,6 +6,7 @@ template = """
           <html>
             <head>
               <title>Paper overview</title>
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
             </head>
             <body>
               <h1>Paper overview</h1>
